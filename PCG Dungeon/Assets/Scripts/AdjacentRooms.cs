386444a4b7d84bdd1eb97c7252090e63f6b07853
Ethan Ball (RIT Student) <ejb3432@rit.edu>
2023-10-13 10:46:05 -0400

    made basic generation work

--- PCG Dungeon/Assets/Scripts/AdjacentRooms.cs
+++ PCG Dungeon/Assets/Scripts/AdjacentRooms.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum RoomType {
     None,
+    SolidWall,
+
     Start,
     Boss,
-    Miniboss,
     Key,
 
     MonsterRoom,
     TreasureRoom,
 
-    SolidWall,
-
     HoriHall,
     VertHall,
+    PlusHall
     //LHall,
     //THall,
-    PlusHall
     // trap hall?
 }
 
@@ -28,14 +28,65 @@ public struct AdjacentRooms {
     public RoomType Left;
     public RoomType Right;
 
-    public AdjacentRooms(RoomType above, RoomType below, RoomType left, RoomType right) {
-        Above = above;
-        Below = below;
-        Left = left;
-        Right = right;
+    public RoomType AboveFurther;
+    public RoomType BelowFurther;
+    public RoomType LeftFurther;
+    public RoomType RightFurther;
+
+    public RoomType[] Diagonals;
+
+    public AdjacentRooms(RoomType[,] roomGrid, int x, int y) {
+        Above = DungeonGenerator.GetTile(roomGrid, x, y + 1);
+        Below = DungeonGenerator.GetTile(roomGrid, x, y - 1);
+        Left = DungeonGenerator.GetTile(roomGrid, x - 1, y);
+        Right = DungeonGenerator.GetTile(roomGrid, x + 1, y);
+
+        AboveFurther = DungeonGenerator.GetTile(roomGrid, x, y + 2);
+        BelowFurther = DungeonGenerator.GetTile(roomGrid, x, y - 2);
+        LeftFurther = DungeonGenerator.GetTile(roomGrid, x - 2, y);
+        RightFurther = DungeonGenerator.GetTile(roomGrid, x + 2, y);
+
+        Diagonals = new RoomType[4] {
+            DungeonGenerator.GetTile(roomGrid, x + 1, y + 1),
+            DungeonGenerator.GetTile(roomGrid, x - 1, y + 1),
+            DungeonGenerator.GetTile(roomGrid, x + 1, y - 1),
+            DungeonGenerator.GetTile(roomGrid, x - 1, y - 1)
+        };
     }
     
-    public bool Contains(RoomType type) {
+    public bool IsAdjacent(RoomType type) {
         return Above == type || Below == type || Left == type || Right == type;
+    }
+
+    public bool TwoConsecutiveRooms() {
+        return DungeonGenerator.IsRoom(Above) && DungeonGenerator.IsRoom(AboveFurther) ||
+            DungeonGenerator.IsRoom(Below) && DungeonGenerator.IsRoom(BelowFurther) ||
+            DungeonGenerator.IsRoom(Left) && DungeonGenerator.IsRoom(LeftFurther) ||
+            DungeonGenerator.IsRoom(Right) && DungeonGenerator.IsRoom(RightFurther);
+    }
+
+    public bool TwoConsecutiveHalls() {
+        return DungeonGenerator.IsHall(Above) && DungeonGenerator.IsHall(AboveFurther) ||
+            DungeonGenerator.IsHall(Below) && DungeonGenerator.IsHall(BelowFurther) ||
+            DungeonGenerator.IsHall(Left) && DungeonGenerator.IsHall(LeftFurther) ||
+            DungeonGenerator.IsHall(Right) && DungeonGenerator.IsHall(RightFurther);
+    }
+
+    public bool BetweenRooms() {
+        return DungeonGenerator.IsRoom(Above) && DungeonGenerator.IsRoom(Below) ||
+            DungeonGenerator.IsRoom(Left) && DungeonGenerator.IsRoom(Right);
+    }
+
+    public bool BetweenHalls() {
+        return DungeonGenerator.IsHall(Above) && DungeonGenerator.IsHall(Below) ||
+            DungeonGenerator.IsHall(Left) && DungeonGenerator.IsHall(Right);
+    }
+
+    public bool HasAdjacentRoom() {
+        return DungeonGenerator.IsRoom(Above) || DungeonGenerator.IsRoom(Below) || DungeonGenerator.IsRoom(Left) || DungeonGenerator.IsRoom(Right);
+    }
+
+    public bool HasAdjacentHall() {
+        return DungeonGenerator.IsHall(Above) || DungeonGenerator.IsHall(Below) || DungeonGenerator.IsHall(Left) || DungeonGenerator.IsHall(Right);
     }
 }
--- PCG Dungeon/Assets/Scripts/DungeonGenerator.cs
+++ PCG Dungeon/Assets/Scripts/DungeonGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DungeonGenerator : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class DungeonGenerator : MonoBehaviour
     private RoomType[] roomTypes;
     [SerializeField] private int Dimensions;
     [SerializeField] private float RoomScale;
+
+    [Header("Order of this list must match the order of Enum RoomType")]
+    [SerializeField] private GameObject[] TilePrefabs;
 
     void Start() {
         roomTypes = (RoomType[])Enum.GetValues(typeof(RoomType));
@@ -20,6 +24,21 @@ public class DungeonGenerator : MonoBehaviour
 
         if(roomGrid == null) {
             Debug.Log("algorithm failed multiple attempts :(");
+        } else {
+            SpawnDungeon();
+        }
+    }
+
+    // places game objects based on the populated room grid
+    private void SpawnDungeon() {
+        float startCoord = (-Dimensions + 1) * RoomScale / 2.0f;
+
+        for(int x = 0; x < Dimensions; x++) {
+            for(int y = 0; y < Dimensions; y++) {
+                GameObject newTile = Instantiate(TilePrefabs[(int)roomGrid[x, y]]);
+                newTile.transform.localScale = new Vector3(RoomScale, RoomScale, 1);
+                newTile.transform.position = new Vector3(startCoord + x * RoomScale, startCoord + y * RoomScale, 0);
+            }
         }
     }
 
@@ -33,8 +52,17 @@ public class DungeonGenerator : MonoBehaviour
             int minOptions = int.MaxValue;
             for(int x = 0; x < Dimensions; x++) {
                 for(int y = 0; y < Dimensions; y++) {
-                    int numOptions = GetTileOptions(x, y).Count;
-                    if(numOptions < minOptions) {
+                    if(roomGrid[x, y] != RoomType.None) {
+                        // do not place a tile in the same spot twice
+                        continue;
+                    }
+
+                    int numOptions = GetTileOptions(roomGrid, x, y).Count;
+                    if(numOptions == 0) {
+                        // algorithm failed
+                        return null;
+                    }
+                    else if(numOptions < minOptions) {
                         // found a more restrictive tile
                         minOptions = numOptions;
                         tileOptions.Clear();
@@ -44,38 +72,29 @@ public class DungeonGenerator : MonoBehaviour
                         // found an equally restrictive tile
                         tileOptions.Add(new Vector2Int(x, y));
                     }
-                    else if(numOptions == 0) {
-                        // algorithm failed
-                        return null;
-                    }
                 }
             }
 
             // place one of the most restrictive tiles
             Vector2Int chosenTile = tileOptions[UnityEngine.Random.Range(0, tileOptions.Count)];
-            List<RoomType> typeOptions = GetTileOptions(chosenTile.x, chosenTile.y);
+            List<RoomType> typeOptions = GetTileOptions(roomGrid, chosenTile.x, chosenTile.y);
             roomGrid[chosenTile.x, chosenTile.y] = typeOptions[UnityEngine.Random.Range(0, typeOptions.Count)];
         }
 
         return roomGrid;
     }
 
-    private RoomType GetTile(int x, int y) {
-        if(x < 0 || y < 0 || x >= Dimensions || y >= Dimensions) {
+    public static RoomType GetTile(RoomType[,] roomGrid, int x, int y) {
+        if(x < 0 || y < 0 || x >= roomGrid.GetLength(1) || y >= roomGrid.GetLength(0)) {
             return RoomType.None;
         }
 
         return roomGrid[x, y];
     }
 
-    private List<RoomType> GetTileOptions(int x, int y) {
+    private List<RoomType> GetTileOptions(RoomType[,] roomGrid, int x, int y) {
         List<RoomType> options = new List<RoomType>();
-        AdjacentRooms adjacents = new AdjacentRooms(
-            GetTile(x, y + 1),
-            GetTile(x, y - 1),
-            GetTile(x - 1, y),
-            GetTile(x + 1, y)
-        );
+        AdjacentRooms adjacents = new AdjacentRooms(roomGrid, x, y);
 
         foreach(RoomType roomType in roomTypes) {
             if(IsRoomValid(roomType, adjacents)) {
@@ -91,17 +110,80 @@ public class DungeonGenerator : MonoBehaviour
         switch(type) {
             case RoomType.Start:
             case RoomType.Boss:
-            case RoomType.Miniboss:
             case RoomType.Key:
                 return false;
 
-            case RoomType.MonsterRoom:
-                if(IsRoom(adjacents.Above) && IsRoom(adjacents.Below) || IsRoom(adjacents.Left) && IsRoom(adjacents.Right)) {
-                    // cannot be the middle of two other rooms
+            case RoomType.SolidWall:
+                if(adjacents.IsAdjacent(RoomType.SolidWall)) {
+                    // solid walls must be 1x1
                     return false;
                 }
-                if(adjacents.Contains(RoomType.TreasureRoom)) {
+                if(HasRightPath(adjacents.Left) || HasLeftPath(adjacents.Right) || HasDownPath(adjacents.Above) || HasUpPath(adjacents.Below)) {
+                    // do not block a hall
+                    return false;
+                }
+                return true;
+
+            case RoomType.MonsterRoom:
+                if(adjacents.BetweenRooms() || adjacents.TwoConsecutiveRooms()) {
+                    // cannot have three rooms in a row
+                    return false;
+                }
+                if(adjacents.IsAdjacent(RoomType.TreasureRoom)) {
                     // no treasure rooms with monster rooms
+                    return false;
+                }
+                return true;
+
+            case RoomType.TreasureRoom:
+                if(adjacents.HasAdjacentRoom()) {
+                    // treasure rooms must be alone
+                    return false;
+                }
+                if (adjacents.AboveFurther == RoomType.TreasureRoom || adjacents.BelowFurther == RoomType.TreasureRoom
+                    || adjacents.LeftFurther == RoomType.TreasureRoom || adjacents.RightFurther == RoomType.TreasureRoom
+                    || adjacents.Diagonals.Contains(RoomType.TreasureRoom)
+                ) {
+                    // no treasure rooms nearby existing treasure rooms
+                    return false;
+                }
+                return true;
+
+            case RoomType.HoriHall:
+                if(HasDownPath(adjacents.Above) || HasUpPath(adjacents.Below) || HasRightBlocked(adjacents.Left) || HasLeftBlocked(adjacents.Right)) {
+                    // no dead ends
+                    return false;
+                }
+                if(IsHall(adjacents.Left) && IsHall(adjacents.Right) ||
+                    IsHall(adjacents.Left) && IsHall(adjacents.LeftFurther) ||
+                    IsHall(adjacents.Right) && IsHall(adjacents.RightFurther)
+                ) {
+                    // cannot have three rooms in a row
+                    return false;
+                }
+                return true;
+
+            case RoomType.VertHall:
+                if(HasRightPath(adjacents.Left) || HasLeftPath(adjacents.Right) || HasUpBlocked(adjacents.Below) || HasDownBlocked(adjacents.Above)) {
+                    // no dead ends
+                    return false;
+                }
+                if(IsHall(adjacents.Above) && IsHall(adjacents.Below) ||
+                    IsHall(adjacents.Above) && IsHall(adjacents.AboveFurther) ||
+                    IsHall(adjacents.Below) && IsHall(adjacents.BelowFurther)
+                ) {
+                    // cannot have three rooms in a row
+                    return false;
+                }
+                return true;
+
+            case RoomType.PlusHall:
+                if(HasRightBlocked(adjacents.Left) || HasLeftBlocked(adjacents.Right) || HasUpBlocked(adjacents.Below) || HasDownBlocked(adjacents.Above)) {
+                    // no dead ends
+                    return false;
+                }
+                if(adjacents.TwoConsecutiveHalls()) {
+                    // cannot have three rooms in a row
                     return false;
                 }
                 return true;
@@ -113,11 +195,11 @@ public class DungeonGenerator : MonoBehaviour
         return false;
     }
 
-    private bool IsRoom(RoomType type) {
+    #region tile type definitions
+    public static bool IsRoom(RoomType type) {
         switch(type) {
             case RoomType.Start:
             case RoomType.Boss:
-            case RoomType.Miniboss:
             case RoomType.Key:
             case RoomType.MonsterRoom:
             case RoomType.TreasureRoom:
@@ -127,7 +209,7 @@ public class DungeonGenerator : MonoBehaviour
         return false;
     }
 
-    private bool IsHall(RoomType type) {
+    public static bool IsHall(RoomType type) {
         switch(type) {
             case RoomType.HoriHall:
             case RoomType.VertHall:
@@ -137,4 +219,87 @@ public class DungeonGenerator : MonoBehaviour
 
         return false;
     }
+
+    // these four define which halls go in which directions, return false for all rooms
+    public static bool HasUpPath(RoomType type) {
+        switch(type) {
+            case RoomType.VertHall:
+            case RoomType.PlusHall:
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool HasDownPath(RoomType type) {
+        switch(type) {
+            case RoomType.VertHall:
+            case RoomType.PlusHall:
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool HasLeftPath(RoomType type) {
+        switch(type) {
+            case RoomType.HoriHall:
+            case RoomType.PlusHall:
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool HasRightPath(RoomType type) {
+        switch(type) {
+            case RoomType.HoriHall:
+            case RoomType.PlusHall:
+                return true;
+        }
+
+        return false;
+    }
+
+    // these four define which tiles cannot have a path connect to each side
+    public static bool HasUpBlocked(RoomType type) {
+        switch(type) {
+            case RoomType.SolidWall:
+            case RoomType.HoriHall:
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool HasDownBlocked(RoomType type) {
+        switch(type) {
+            case RoomType.SolidWall:
+            case RoomType.HoriHall:
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool HasLeftBlocked(RoomType type) {
+        switch(type) {
+            case RoomType.SolidWall:
+            case RoomType.VertHall:
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool HasRightBlocked(RoomType type) {
+        switch(type) {
+            case RoomType.SolidWall:
+            case RoomType.VertHall:
+                return true;
+        }
+
+        return false;
+    }
+    #endregion
 }
